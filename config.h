f33a5c9889ca91ca10b9b2b9ac1dd9e6df3e0a32
Yongun Seong <nevivurn@riseup.net>
2019-11-03 01:56:24 +0900

    Fix util ratio

--- config.h
+++ config.h
@@ -61,7 +61,7 @@
   * (UTIL_WEIGHT) and throughput (1 - UTIL_WEIGHT) to the performance
   * index.
   */
-#define UTIL_WEIGHT .70
+#define UTIL_WEIGHT .60
 
 /*
  * Alignment requirement in bytes (either 4 or 8)
